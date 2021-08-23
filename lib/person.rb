f905d9b6874512efa1132da214cada17ef2775a8
Harold Torres Marino <haroldtm55@gmail.com>
2021-08-23 15:27:23 +1000

    commit tree e15ee23a1bdb7eada970eb7a946142832b52fa58

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,3 @@
-# attributes = [:name, :birthday, :hair_color, :eye_color, :height, 
-#   :weight, :handed, :complexion, :t_shirt_size, 
-#   :wrist_size, :glove_size, :pant_length, :pant_width]
-
 class Person
   
   def initialize(attributes)
@@ -12,21 +8,3 @@ class Person
   end
 
 end
-
-avi_attributes = {
-  :name => "Avi",
-  :birthday => "01/29/1984",
-  :hair_color => "brown",
-  :eye_color => "brown",
-  :height => "short",
-  :weight => "good",
-  :handed => "lefty",
-  :complexion => "decent",
-  :t_shirt_size => "medium",
-  :wrist_size => "small",
-  :glove_size => "normal",
-  :pant_length => "32",
-  :pant_width => "32"
-}
-
-Person.new(avi_attributes)
